d2db2b2b155f91485660820ff74a22a95677a105
kushikushi5969 <yuya.kushida1221@gmail.com>
2023-10-12 01:07:38 +0900

    docs: change FV URL

--- components/fv.js
+++ components/fv.js
@@ -18,8 +18,8 @@ function fv() {
 
   return (
     <div className="p-fv">
-      <div id="video-pc" style={positionPc}><iframe src="https://player.vimeo.com/video/873331635?autoplay=1&amp;loop=1&amp;muted=1&amp;badge=0&amp;autopause=0&amp;quality_selector=1&amp;progress_bar=1&amp;player_id=0&amp;app_id=58479" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" style={iframe} title="fv_pc"></iframe></div>
-      <div id="video-sp" style={positionSp}><iframe src="https://player.vimeo.com/video/873331566?autoplay=1&amp;loop=1&amp;muted=1&amp;badge=0&amp;autopause=0&amp;quality_selector=1&amp;progress_bar=1&amp;player_id=0&amp;app_id=58479" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" style={iframe} title="fv_sp"></iframe></div>
+      <div id="video-pc" style={positionPc}><iframe src="https://player.vimeo.com/video/873331635?background=1" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" style={iframe} title="fv_pc"></iframe></div>
+      <div id="video-sp" style={positionSp}><iframe src="https://player.vimeo.com/video/873331566?background=1" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" style={iframe} title="fv_sp"></iframe></div>
       <script src="https://player.vimeo.com/api/player.js"></script>
     </div>
   );
