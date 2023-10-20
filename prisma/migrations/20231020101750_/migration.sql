0f37095fe7ab79a46f4bf5eb585b0065758d629e
kussy <59913169+kushikushi5969@users.noreply.github.com>
2023-10-20 19:38:03 +0900

    feat: initialize supabase (#22)
    * feat: install supabase
    
    * feat: initialize supabase

---
+++ prisma/migrations/20231020101750_/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Ticket" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "count" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Ticket_pkey" PRIMARY KEY ("id")
+);
