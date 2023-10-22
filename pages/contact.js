8ec654c3200f813cd975388f09e8367d742daa0e
kushikushi5969 <yuya.kushida1221@gmail.com>
2023-10-22 15:32:32 +0900

    feat: add canonical

--- pages/contact.js
+++ pages/contact.js
@@ -9,6 +9,8 @@ export default function contact() {
       <Head>
         <title>BUTTERFLY EFFECT</title>
         <link rel="icon" href="/favicon-butterflyeffect.png" />
+        <link rel="apple-touch-icon" href="/favicon-butterflyeffect.png" />
+        <link rel="canonical" href="https://butterflyeffect2023.com/contact" />
         <meta property="og:title" content="BUTTERFLY EFFECT" />
         <meta property="og:site_name" content="BUTTERFLY EFFECT" />
         <meta property="og:description" content="2010年、高校一年生だった自分は、同級生とたった二人で人生初めてのバンドを結成した。「白熊ピカソズ」、そんなヘンテコな名前の、湘南の片隅のパンクバンドから自分の全てが始まった。" />
