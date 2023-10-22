7081b98fbf077131bfdcadb4f7a36b3014fdaade
kushikushi5969 <yuya.kushida1221@gmail.com>
2023-10-22 13:56:10 +0900

    docs: add twitter

--- pages/contact.js
+++ pages/contact.js
@@ -10,10 +10,16 @@ export default function contact() {
         <title>BUTTERFLY EFFECT</title>
         <link rel="icon" href="/favicon-butterflyeffect.png" />
         <meta property="og:title" content="BUTTERFLY EFFECT" />
+        <meta property="og:site_name" content="BUTTERFLY EFFECT" />
         <meta property="og:description" content="2010年、高校一年生だった自分は、同級生とたった二人で人生初めてのバンドを結成した。「白熊ピカソズ」、そんなヘンテコな名前の、湘南の片隅のパンクバンドから自分の全てが始まった。" />
         <meta property="og:image" content="https://butterflyeffect2023.com/image/butterflyeffect.jpg" />
         <meta property="og:url" content="https://butterflyeffect2023.com/" />
         <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="BUTTERFLY EFFECT" />
+        <meta name="twitter:description" content="2010年、高校一年生だった自分は、同級生とたった二人で人生初めてのバンドを結成した。「白熊ピカソズ」、そんなヘンテコな名前の、湘南の片隅のパンクバンドから自分の全てが始まった。" />
+        <meta name="twitter:image" content="https://butterflyeffect2023.com/image/butterflyeffect.jpg" />
+        <meta name="twitter:url" content="https://butterflyeffect2023.com/" />
       </Head>
 
       <main className="l-lp">
